552d822ce13d80700c8221be8480a5da21fc88ca
Arthur <arthur@arthurvardevanyan.com>
2023-01-31 22:25:31 -0500

    GKE AutoPilot ReCreate

--- CloudLab/gke-cluster/main.tf
+++ CloudLab/gke-cluster/main.tf
@@ -16,7 +16,8 @@ data "vault_generic_secret" "gke-cluster" {
 locals {
   project_id = data.vault_generic_secret.gke-cluster.data["project_id"]
   # tenant      = data.vault_generic_secret.gke-cluster.data["tenant_cloudbuild"]
-  # tenant_user = data.vault_generic_secret.gke-cluster.data["tenant_user"]
+  #tenant_user = data.vault_generic_secret.gke-cluster.data["tenant_user"]
+  user = data.vault_generic_secret.gke-cluster.data["user"]
   node_roles = [
     "roles/logging.logWriter",
     "roles/monitoring.metricWriter",
@@ -37,50 +38,59 @@ resource "google_project_service" "gkehub" {
   service            = "gkehub.googleapis.com"
   disable_on_destroy = false
 }
-
-# resource "google_project_iam_custom_role" "gke_tenant" {
-#   project = "gke-cluster-${local.project_id}"
-#   role_id = "gke.tenant"
-#   title   = "GKE Tenant"
-#   permissions = [
-#     "container.apiServices.get",
-#     "container.apiServices.list",
-#     "container.clusters.get",
-#     "container.clusters.getCredentials",
-#     "container.clusters.list",
-#     "monitoring.timeSeries.list",
-#     # Cloud Resources
-#     "resourcemanager.projects.get",
-#   ]
-# }
-
-# resource "google_project_iam_binding" "gke_tenant" {
-#   project = "gke-cluster-${local.project_id}"
-#   role    = resource.google_project_iam_custom_role.gke_tenant.id
-
-#   members = [
-#     "serviceAccount:${local.tenant}@cloudbuild.gserviceaccount.com",
-#     "user:${local.tenant_user}",
-#   ]
-# }
-
-
-resource "google_monitoring_monitored_project" "primary" {
-  metrics_scope = "locations/global/metricsScopes/gke-tenant-${local.project_id}"
-  name          = "locations/global/metricsScopes/gke-tenant-${local.project_id}/projects/gke-cluster-${local.project_id}"
+resource "google_project_service" "containersecurity" {
+  project            = "gke-cluster-${local.project_id}"
+  service            = "containersecurity.googleapis.com"
+  disable_on_destroy = false
 }
 
-resource "google_logging_project_sink" "gke-autopilot" {
-  name    = "gke-autopilot"
+resource "google_project_iam_custom_role" "gke_tenant" {
   project = "gke-cluster-${local.project_id}"
-
-  description = "Log to gke-tenants Project"
-  destination = "logging.googleapis.com/projects/gke-tenant-${local.project_id}/locations/global/buckets/_Default"
-
-  filter = <<EOH
-          (resource.type = k8s_container) OR (resource.type= k8s_cluster)
-
-          EOH
-
-  unique_writer_identity = true
+  role_id = "gke.tenant"
+  title   = "GKE Tenant"
+  permissions = [
+    "container.apiServices.get",
+    "container.apiServices.list",
+    "container.clusters.get",
+    "container.clusters.getCredentials",
+    "container.clusters.list",
+    "monitoring.timeSeries.list",
+    # Cloud Resources
+    "resourcemanager.projects.get",
+    # GKE Protect
+    "containersecurity.clusterSummaries.list",
+    "containersecurity.findings.list"
+  ]
 }
+
+resource "google_project_iam_binding" "gke_tenant" {
+  project = "gke-cluster-${local.project_id}"
+  role    = resource.google_project_iam_custom_role.gke_tenant.id
+
+  members = [
+    #"serviceAccount:${local.tenant}@cloudbuild.gserviceaccount.com",
+    #"user:${local.tenant_user}",
+    "user:${local.user}",
+  ]
+}
+
+
+# resource "google_monitoring_monitored_project" "primary" {
+#   metrics_scope = "locations/global/metricsScopes/gke-tenant-${local.project_id}"
+#   name          = "locations/global/metricsScopes/gke-tenant-${local.project_id}/projects/gke-cluster-${local.project_id}"
+# }
+
+# resource "google_logging_project_sink" "gke-autopilot" {
+#   name    = "gke-autopilot"
+#   project = "gke-cluster-${local.project_id}"
+
+#   description = "Log to gke-tenants Project"
+#   destination = "logging.googleapis.com/projects/gke-tenant-${local.project_id}/locations/global/buckets/_Default"
+
+#   filter = <<EOH
+#           (resource.type = k8s_container) OR (resource.type= k8s_cluster)
+
+#           EOH
+
+#   unique_writer_identity = true
+# }
--- CloudLab/gke-cluster/outputs.tf
+++ CloudLab/gke-cluster/outputs.tf
@@ -1,4 +1,4 @@
-output "logging_sa" {
-  value       = resource.google_logging_project_sink.gke-autopilot.writer_identity
-  description = "GKE Autopilot SA Writer Identity"
-}
+# output "logging_sa" {
+#   value       = resource.google_logging_project_sink.gke-autopilot.writer_identity
+#   description = "GKE Autopilot SA Writer Identity"
+# }
